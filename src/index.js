e93a9690bd8d512e4b22f2b17789b6220fd4a9c8
Naveen12345-alt <naveenchauhan1234@protonmail.com>
2021-05-31 00:34:55 +0530

    react-snap

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 //REDUX SETUP
-import { BrowserRouter } from 'react-router-dom';
-import /*webpackPrefetch:true*/ App from './App';
+import {BrowserRouter} from 'react-router-dom'
+import /*webpackPrefetch:true*/ App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
+const rootElement = document.getElementById('root')
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(
     <BrowserRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    </BrowserRouter>,
+    rootElement,
+  )
+} else {
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootElement,
+  )
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
